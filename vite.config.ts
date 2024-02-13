5ea8067f298a6667e8597114c5ad8b07003f9d66
Juliette Lamarche <princesse@princesse.xyz>
2024-02-13 15:17:46 -0500

    ci/cd: pages config

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,7 @@ const mdxOptions: any = {
 // https://vitejs.dev/config/
 export default defineConfig({
   /* plugins: [{ enforce: "pre", ...mdx(mdxOptions) }, react()], */
+  base: "optics-talk",
   plugins: [
     { enforce: "pre", ...mdx(mdxOptions) },
     Unfonts({
